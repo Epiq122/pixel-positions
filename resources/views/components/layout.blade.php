f2993b8e433ab92705f00b68e066381fc3f3f418
Robert Gleason <l2ob122@gmail.com>
2024-07-05 06:56:01 -0700

    DESIGN: created a card of job wide and began styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,7 @@
     <title>Pixel Positions</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
     <link
         href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap"
         rel="stylesheet">
