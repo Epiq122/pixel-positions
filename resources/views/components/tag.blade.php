e5711bd307af0bc0c395f04a3e819aaf596ff98a
Robert Gleason <l2ob122@gmail.com>
2024-07-08 21:41:36 -0700

    DESIGN:created seperate classes for different tag sizes

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,3 +1,16 @@
+@props(['size' => 'base'])
+
+@php
+    $classes="bg-white/10 hover:bg-white/25 rounded-xl font-bold x-3 transition-colors duration-300";
+    if($size === 'base'){
+            $classes .= " px-5 py-1 text-sm";
+
+    }
+       if($size === 'small'){
+            $classes .= " px-2 py-1 text-2xs";
+    }
+@endphp
+
 <a href="#"
-   class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-2xs font-bold x-3 transition-colors duration-300">{{$slot}}</a>
+   class="{{$classes}}">{{$slot}}</a>
 
