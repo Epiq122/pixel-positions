b5085d5f5cbe05b2b4d7a40dc7ed8ab090629d78
Robert Gleason <l2ob122@gmail.com>
2024-07-10 19:23:26 -0700

    LOGIC(JOB):got rid off all the static data and got our data from the backend

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -6,14 +6,14 @@
     </div>
 
     <div class="flex-1  flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Epiq Dev</a>
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300 ">Editor</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $50,000</p>
+        <a href="#" class="self-start text-sm text-gray-400">{{$job->employer->name}}</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300 ">{{$job->title}}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{$job->salary}}</p>
     </div>
 
     <div>
         @foreach($job->tags as $tag)
-            <x-tag :$tag>Backend</x-tag>
+            <x-tag :$tag/>
 
         @endforeach
     </div>
