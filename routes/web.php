38ebd33f2468503365b0a1017f228397b9a5124d
Robert Gleason <l2ob122@gmail.com>
2024-07-10 21:06:22 -0700

    FORM:created the registration form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+
+//AUTH
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::get('/login', [SessionController::class, 'store']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/login', [SessionController::class, 'destroy']);
+
